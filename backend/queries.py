b0dec462d923e0fe2762517c53cb00bb711960fa
kaylynn-johnson <kaylynnrogers3@gmail.com>
2026-08-10 21:07:22 -0600

    Query for search filters

--- backend/queries.py
+++ backend/queries.py
@@ -34,3 +34,43 @@ def habitable_planets():
     result = result.replace([np.inf, -np.inf, np.nan], None).to_dict(orient="records")
 
     return result
+
+
+def search_planets(filters: dict, limit: int, offset: int):
+
+    clauses = []
+    parameters = {}
+
+    if filters["radius_min"] is not None:
+        clauses.append("pl_rade >= $radius_min")
+        parameters["radius_min"] = filters["radius_min"]
+
+    if filters["radius_max"] is not None:
+        clauses.append("pl_rade <= $radius_max")
+        parameters["radius_max"] = filters["radius_max"]
+
+    if filters["orbit_period_min"] is not None:
+        clauses.append("pl_orbper >= $orbit_period_min")
+        parameters["orbit_period_min"] = filters["orbit_period_min"]
+
+    if filters["orbit_period_max"] is not None:
+        clauses.append("pl_orbper <= $orbit_period_max")
+        parameters["orbit_period_max"] = filters["orbit_period_max"]
+
+    if filters["discovery_method"] is not None:
+        clauses.append("discoverymethod = $discovery_method")
+        parameters["discovery_method"] = filters["discovery_method"]
+
+    if filters["spectral_type"] is not None:
+        clauses.append("stellar_category = $spectral_type")
+        parameters["spectral_type"] = filters["spectral_type"]
+
+    # last bit is if there are no parameters passed
+    where_clause = " AND ".join(clauses) if clauses else "1=1"
+    search_query = f"SELECT * FROM planets WHERE {where_clause} LIMIT $limit OFFSET $offset"
+    parameters["limit"] = limit
+    parameters["offset"] = offset
+    result = con.execute(search_query, parameters).fetchdf()
+    result = result.replace([np.inf, -np.inf, np.nan], None).to_dict(orient="records")
+
+    return result
