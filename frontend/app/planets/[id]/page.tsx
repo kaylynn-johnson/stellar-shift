25e74533ef9e139afe56a7aa3f9ee11beae1ef6a
kaylynn-johnson <kaylynnrogers3@gmail.com>
2026-08-11 21:25:35 -0600

    Start of routes for frontend, closes #8

---
+++ frontend/app/planets/[id]/page.tsx
@@ -0,0 +1,6 @@
+export default async function PlanetId(searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+) {
+    return (
+        <div>This is the planet information</div>
+    )
+}
---
+++ frontend/app/planets/page.tsx
@@ -0,0 +1,7 @@
+export default async function Planets() {
+    return (
+        <div>
+            <h1>This is the planets route</h1>
+        </div>
+    )
+}
